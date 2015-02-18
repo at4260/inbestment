14a8e5699b4f2afcdfa8bde0c2b5fea31f07591e
at4260 <at4260@gmail.com>
2015-02-17 18:03:43 -0800

    data.py refactored with ticker list, instead of ticker list within a function

--- seed_data/data.py
+++ seed_data/data.py
@@ -5,40 +5,40 @@
 
 import json, urllib
 
-def build_ticker_url(ticker):
+ticker_list = ["MX_FB", "MX_COST"]
+ticker_url_list = []
+
+def build_ticker_url(ticker_list):
 	""" Queries the url using the desired ticker and token"""
-	url = "https://www.quandl.com/api/v1/datasets/YAHOO/"
-	token = open("../tokens.txt").read()
-	ticker_url = url + ticker + ".json?auth_token=" + token
-	return ticker_url
+	for ticker in ticker_list:
+		url = "https://www.quandl.com/api/v1/datasets/YAHOO/"
+		token = open("../tokens.txt").read()
+		ticker_url = url + ticker + ".json?auth_token=" + token
+		ticker_url_list.append(ticker_url)
+	return ticker_url_list
 
 
-def get_ticker_data(ticker_url):	
-	u = urllib.urlopen(ticker_url)
-	data = u.read()
-	newdata = json.loads(data)
+def get_ticker_data(ticker_url_list):
+	for ticker_url in ticker_url_list:	
+		u = urllib.urlopen(ticker_url)
+		data = u.read()
+		newdata = json.loads(data)
 
-	# FIXME- "code" pulls the url query, not the actual symbol
-	ticker_symbol = newdata["code"]
-	ticker_name = newdata["name"]
-	print ticker_symbol
-	print ticker_name
+		# FIXME- "code" pulls the url query, not the actual symbol
+		ticker_symbol = newdata["code"]
+		ticker_name = newdata["name"]
+		print ticker_symbol
+		print ticker_name
 
-	# prices pulls a list of lists (consisting of date, open, high, low, close, volume. adjusted close)
-	prices = newdata["data"]
+		# prices pulls a list of lists (consisting of date, open, high, low, close, volume. adjusted close)
+		prices = newdata["data"]
 
-	# itierate over the list of lists to pull out the individual list of date data
-	for price in prices:
-	    print price[0], ":", price[4]
-
-def get_tickers():
-	"""	This returns a static list of tickers.
-	In the future this should read the list of existing tickers from the database. """
-	return ["MX_FB", "MX_COST"]
+		# itierate over the list of lists to pull out the individual list of date data
+		for price in prices:
+		    print price[0], ":", price[4]
 
 def main():
-	for ticker in get_tickers():
-		return get_ticker_data(build_ticker_url(ticker))
+	get_ticker_data(build_ticker_url(ticker_list))
 
 if __name__ == "__main__":
 	main()
