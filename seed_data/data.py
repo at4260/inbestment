d4f63b04ca1520597829856c992130ee0c036cc2
at4260 <at4260@gmail.com>
2015-02-17 17:38:12 -0800

    data.py refactored with functions

--- seed_data/data.py
+++ seed_data/data.py
@@ -1,31 +1,45 @@
-# Purpose: pulling API data into multiple data sets
-# data set options: CSV, XML, JSON
-# combine all stock ticker price data into one file and save into seed_date
+""" Pulls QUANDL API data and populates database. """
 
-# TO DO: successfully, pulls the data I want from the JSON file, need to save that data.. can either 1) directly session.commit it to the database (unscrubbed data) or 2) form a giant data file to then pull and scrub through for inputting into database?
+# FIXME: successfully, pulls the data I want from the JSON file, need to 
+# directly session.commit it to the database (unscrubbed data) 
+
 import json, urllib
 
-ticker_dict = ["MX_FB", "MX_COST"]
+def build_ticker_url(ticker):
+	""" Queries the url using the desired ticker and token"""
+	url = "https://www.quandl.com/api/v1/datasets/YAHOO/"
+	token = open("../tokens.txt").read()
+	ticker_url = url + ticker + ".json?auth_token=" + token
+	return ticker_url
 
-# queries the url using the desired ticker and token
-url = "https://www.quandl.com/api/v1/datasets/YAHOO/"
-token = open("../tokens.txt").read()
-ticker_url = url + ticker_dict[0] + ".json?auth_token=" + token
 
-# load the JSON data as a dictionary
-u = urllib.urlopen(ticker_url)
-data = u.read()
-newdata = json.loads(data)
+def get_ticker_data(ticker_url):	
+	u = urllib.urlopen(ticker_url)
+	data = u.read()
+	newdata = json.loads(data)
 
-# FIXME- "code" pulls the url query, not the actual symbol
-ticker_symbol = newdata["code"]
-ticker_name = newdata["name"]
-print ticker_symbol
-print ticker_name
+	# FIXME- "code" pulls the url query, not the actual symbol
+	ticker_symbol = newdata["code"]
+	ticker_name = newdata["name"]
+	print ticker_symbol
+	print ticker_name
 
-# prices pulls a list of lists (consisting of date, open, high, low, close, volume. adjusted close)
-prices = newdata["data"]
+	# prices pulls a list of lists (consisting of date, open, high, low, close, volume. adjusted close)
+	prices = newdata["data"]
 
-# itierate over the list of lists to pull out the individual list of date data
-for price in prices:
-    print price[0], ":", price[4]
+	# itierate over the list of lists to pull out the individual list of date data
+	for price in prices:
+	    print price[0], ":", price[4]
+
+def get_tickers():
+	"""	This returns a static list of tickers.
+	In the future this should read the list of existing tickers from the database. """
+	return ["MX_FB", "MX_COST"]
+
+def main():
+	for ticker in get_tickers():
+		return get_ticker_data(build_ticker_url(ticker))
+
+if __name__ == "__main__":
+	main()
+	
