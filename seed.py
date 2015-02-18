84f23e4bda3ef15e32ca134a97cc965638cf6d92
at4260 <at4260@gmail.com>
2015-02-17 21:05:58 -0800

    renamed data.py to seed.py

--- seed.py
+++ seed.py
@@ -1,61 +1,58 @@
-""" Autoload database with data.py data """
+""" Pulls QUANDL API data and autoloads database """
 
+import json, urllib
 import model
 from model import Ticker, Price
 from datetime import datetime
-import data
 
-def load_tickers(session):
-        name_dict = data.get_ticker_names(data.build_ticker_url(data.ticker_list))
+# FIXME: need to input real tickers in ticker list
+ticker_list = ["MX_FB", "MX_COST"]
+ticker_url_list = []
 
-        user_age = data[1]
-        user_zipcode = data[4]
-        # create new instance of the User class in model.py called my_new_user, set class attributes to variables defined in this function
-        new_ticker = Ticker(symbol=user_age, name=user_zipcode)
+def build_ticker_url(ticker_list):
+	""" Queries the url using the desired ticker and token"""
+	for ticker in ticker_list:
+		url = "https://www.quandl.com/api/v1/datasets/YAHOO/"
+		token = open("tokens.txt").read()
+		ticker_url = url + ticker + ".json?auth_token=" + token
+		ticker_url_list.append(ticker_url)
+	return ticker_url_list
+
+def load_ticker_data(ticker_url_list, session):
+	for ticker_url in ticker_url_list:	
+		u = urllib.urlopen(ticker_url)
+		data = u.read()
+		newdata = json.loads(data)
+
+		# FIXME- "code" pulls the url query, not the actual symbol
+		ticker_symbol = newdata["code"]
+		ticker_name = newdata["name"]
+		
+		# prices pulls a list of lists (consisting of date, open, high, 
+			# low, close, volume. adjusted close)
+		prices = newdata["data"]
+
+		for price in prices:
+			date = price[0]
+			date_format = datetime.strptime(date, "%Y-%m-%d")
+			date_format = date_format.date()
+			close_price = price[4]
+			new_ticker_price = Price(ticker_symbol=ticker_symbol, 
+				date=date_format, close_price=close_price)
+			session.add(new_ticker_price)
+
+		# create new instance of the Ticker class called new_ticker
+		new_ticker = Ticker(symbol=ticker_symbol, name=ticker_name)
         # add each instance to session
-        session.add(new_ticker)
+		session.add(new_ticker)
     # commit after all instances are added
-    session.commit()
-
-
-def load_prices(session):
-    # use u.item
-    filename=("./seed_data/u.item")
-    # read file and parse using csv method
-    with open(filename, 'rb') as csvfile:
-        openfile = csv.reader(csvfile, delimiter='|')
-        for row in openfile:
-            movie_title = row[1]
-            # convert latin-1 formatting (with special characters) to unicode, allowing SQLAlchemy to work
-            movie_title = movie_title.decode("latin-1")
-
-            # slicing release year out of movie title
-            if "(" not in movie_title:
-                movie_title = movie_title
-            else:
-                movie_title = movie_title[:-7]
-
-            movie_release = row[2]
-            # convert data to datetime only when release date is not empty; if empty, set to None
-            if movie_release != "":
-                movie_date = datetime.strptime(movie_release, "%d-%b-%Y")
-            else:
-                movie_date = None
-
-            # if url doesn't exist, add "" (okay because field is nullable)
-            movie_url = row[4]
-            my_new_movie = Movie(title=movie_title, release_date=movie_date, url=movie_url)
-            session.add(my_new_movie)
-        session.commit() 
+	session.commit()
 
 
 def main(session):
-#     # You'll call each of the load_* functions with the session as an argument
-    load_tickers(session)
-#     # load_movies(session)
-#     # load_ratings(session)
-#     pass
+	load_ticker_data(build_ticker_url(ticker_list), session)
 
 if __name__ == "__main__":
-    s= model.connect()
-    main(s)
+    session = model.session
+    main(session)
+	
