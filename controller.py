24a9d7b3c88a4024eb3a9491df7b5fb9c1694a22
at4260 <at4260@gmail.com>
2015-02-23 15:32:16 -0800

    show_results function (calculations) works

--- controller.py
+++ controller.py
@@ -23,40 +23,78 @@ def format_currency(value):
 def show_results():
 	assets = float(request.args.get("assets"))
 	income = float(request.args.get("income"))
-	monthly_expenses = (income * (0.28 + 0.36 + 0.10)) / 12
-	checking_needed = monthly_expenses * 1
-	savings_needed = monthly_expenses * 3
-	
 	comp_401k = request.args.get("401k")
 	match_401k = request.args.get("match")
 	match_percent = float(request.args.get("match_percent"))
 	match_salary = float(request.args.get("salary_percent"))
-	if match_401k == "yes":
-		max_match = income * match_percent * match_salary
-		max_401k = 18000 - max_match
+
+	monthly_expenses = (income * (0.28 + 0.36 + 0.10)) / 12
+	checking_needed = monthly_expenses * 1
+	savings_needed = monthly_expenses * 3
+
+	if assets >= checking_needed:
+		assets = assets - checking_needed
 	else:
-		if comp_401k == "yes":
-			max_match = 0
-			max_401k = 18000
+		checking_needed = assets
+		assets = assets - checking_needed
+
+	if assets >= savings_needed:
+		assets = assets - savings_needed
+	else:
+		savings_needed = assets
+		assets = assets - savings_needed
+
+	match_needed = income * match_percent * match_salary
+
+	if comp_401k == "yes":
+		if match_401k == "yes":
+			if assets >= match_needed:
+				assets = assets - match_needed
+			else:
+				match_needed = assets
+				assets = assets - match_needed
 		else:
-			max_match = 0
-			max_401k = 0
-	ira_needed = 5500   
-	investment_acct = assets - checking_needed - savings_needed - max_match - max_401k - ira_needed
-	if investment_acct >= 0:
-		investment_needed = investment_acct
+			match_needed = 0
 	else:
-		investment_needed = 0
-	
-	risk_tolerance = request.args.get("risk_tolerance")
-	risk_profile_id = m_session.query(model.RiskProfile).filter_by(name = risk_tolerance).one().id
+		match_needed = 0
 
-	# FIXME - user id should be reliant on login info
-	new_user_profile = model.UserProfile(user_id=1, income=income, company_401k=comp_401k, company_match=match_401k, match_percent=match_percent, match_salary=match_salary, risk_profile_id=risk_profile_id)
-	m_session.add(new_user_profile)
-	m_session.commit()
+	ret401k_needed = 18000 - match_needed
+	ira_needed = 5500   
 
-	return render_template("results.html", checking=format_currency(checking_needed), savings=format_currency(savings_needed), max_match=format_currency(max_match), max_401k=format_currency(max_401k), ira=format_currency(ira_needed), investment=format_currency(investment_needed))
+	if assets >= ira_needed:
+		assets = assets - ira_needed
+	else:
+		ira_needed = assets
+		assets = assets - ira_needed
+
+	if comp_401k == "yes":
+		if assets >= ret401k_needed:
+			assets = assets - ret401k_needed
+		else:
+			ret401k_needed = assets
+			assets = assets - ret401k_needed
+	else:
+		ret401k_needed = 0
+
+	investment_needed = assets
+	assets = assets - investment_needed
+
+	# risk_tolerance = request.args.get("risk_tolerance")
+	# risk_profile_id = m_session.query(model.RiskProfile).filter_by(name = risk_tolerance).one().id
+
+	# # FIXME - user id should be reliant on login info
+	# new_user_profile = model.UserProfile(user_id=1, income=income, company_401k=comp_401k, company_match=match_401k, match_percent=match_percent, match_salary=match_salary, risk_profile_id=risk_profile_id)
+	# m_session.add(new_user_profile)
+	# m_session.commit()
+
+	return render_template("results.html", 
+		assets=format_currency(assets), 
+		checking=format_currency(checking_needed),
+		savings=format_currency(savings_needed), 
+		match=format_currency(match_needed),
+		ira=format_currency(ira_needed),
+		ret401k=format_currency(ret401k_needed), 
+		investment=format_currency(investment_needed))
 
 @app.route("/investments")
 def show_investments():
