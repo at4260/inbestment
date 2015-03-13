d9db5c45e3c733407a1c6ba53c9a318b9895811e
at4260 <at4260@gmail.com>
2015-03-12 21:32:00 -0700

    Installed pep8 and corrected seed.py.

--- seed.py
+++ seed.py
@@ -1,7 +1,10 @@
-""" Autoloads database with Quandl API data, risk profiles, profile allocations, fake user profiles and banking information """
+"""
+Autoloads database with Quandl API data, risk profiles, profile
+allocations, fake user profiles and banking information.
+"""
 
 import csv
-import json 
+import json
 import model
 import requests
 
@@ -10,9 +13,10 @@ from model import session as m_session
 
 ticker_list = ["VV", "VB", "VEU", "BIV", "BSV", "VWO", "BND"]
 
+
 def find_ticker(ticker_list, file_name):
 	"""
-	Searches through csv file to find ticker and corresponding 
+	Searches through csv file to find ticker and corresponding
 	ticker url identifier.
 	"""
 	ticker_identifier_list = []
@@ -24,12 +28,13 @@ def find_ticker(ticker_list, file_name):
 				ticker_identifier_list.append(data[1])
 	return ticker_identifier_list
 
+
 def build_ticker_url(ticker_identifier_list):
-	""" 
+	"""
 	Queries the url using the desired ticker identifier and token.
 
-	Trims the data set by start date. All values are being benchmarked 
-	against 4/10/2007, which is the latest inception date for all of 
+	Trims the data set by start date. All values are being benchmarked
+	against 4/10/2007, which is the latest inception date for all of
 	the funds for apples-to-apples since-inception comparison.
 	"""
 	ticker_url_list = []
@@ -41,6 +46,7 @@ def build_ticker_url(ticker_identifier_list):
 		ticker_url_list.append(ticker_url)
 	return ticker_url_list
 
+
 def load_ticker_data(ticker_url_list, session):
 	"""
 	This function loads the tickers table and the daily close prices
@@ -48,7 +54,7 @@ def load_ticker_data(ticker_url_list, session):
 
 	Both pull data from the Quandl API as a JSON object.
 	"""
-	for ticker_url in ticker_url_list:	
+	for ticker_url in ticker_url_list:
 		u = requests.get(ticker_url)
 		data = u.text
 		newdata = json.loads(data)
@@ -65,13 +71,13 @@ def load_ticker_data(ticker_url_list, session):
 			ticker_category = "Bonds"
 
 		# Create new instance of the Ticker class called new_ticker
-		new_ticker = model.Ticker(symbol=ticker_symbol, name=ticker_name,
-			category=ticker_category)
-        # Add each instance to session
+		new_ticker = model.Ticker(symbol=ticker_symbol, 
+					name=ticker_name, category=ticker_category)
+		# Add each instance to session
 		session.add(new_ticker)
 		session.commit()
 
-		# Prices pulls a list of lists (consisting of date, open, high, 
+		# Prices pulls a list of lists (consisting of date, open, high,
 		# low, close, volume. adjusted close).
 		prices = newdata["data"]
 
@@ -80,51 +86,55 @@ def load_ticker_data(ticker_url_list, session):
 			date_format = datetime.strptime(date, "%Y-%m-%d")
 			date_format = date_format.date()
 			close_price = price[4]
-			new_ticker_price = model.Price(ticker_id=new_ticker.id, date=date_format, 
-				close_price=close_price)
+			new_ticker_price = model.Price(ticker_id=new_ticker.id, 
+						date=date_format, close_price=close_price)
 			session.add(new_ticker_price)
 
-    # commit after all instances are added
+	# commit after all instances are added
 	session.commit()
+
 
 def calc_percent_change_all(ticker_list, session):
 	"""
 	This function calculates the percent change since 4/10/2007 for all
-	loaded tickers and saves that value to the new_change column in the 
+	loaded tickers and saves that value to the new_change column in the
 	database.
 
 	All values are being benchmarked against 4/10/2007, which is the latest
 	inception date for all of the funds for apples-to-apples since-inception
-	comparison.	
+	comparison.
 	"""
 	ticker_id = 0
 
 	for ticker_data in range(len(ticker_list)):
 		ticker_id = ticker_id + 1
-		ticker = m_session.query(model.Price).filter_by(ticker_id=
-			ticker_id).all()		
-		
-		old_close_price = m_session.query(model.Price).filter_by(date=
-				"2007-04-10", ticker_id=ticker_id).first().close_price
+		ticker = m_session.query(model.Price).filter_by(
+					ticker_id=ticker_id).all()	
+
+		old_close_price = m_session.query(model.Price).filter_by(
+					date="2007-04-10", ticker_id=ticker_id).first().close_price
 		new_index = 0
 
 		for daily_ticker_price in ticker:
 			new_close_price = ticker[new_index].close_price
-			difference = round((new_close_price - old_close_price)/
-				old_close_price, 4)
+			difference = round((new_close_price - old_close_price) / 
+						old_close_price, 4)
 			new_change_id = ticker[new_index].id
-			new_change = m_session.query(model.Price).filter_by(id=
-				new_change_id).update({model.Price.percent_change: difference}) 
+			new_change = m_session.query(model.Price).filter_by(
+						id=new_change_id).update({
+						model.Price.percent_change: difference})
 			new_index = new_index + 1
 		session.commit()
+
 
 def load_risk_profs(session):
 	filename = open("./seed_data/risk_profiles.csv")
 	for line in filename:
 		name = line.strip()
-	 	new_risk_profile = model.RiskProfile(name=name)
+		new_risk_profile = model.RiskProfile(name=name)
 		session.add(new_risk_profile)
 	session.commit()
+
 
 def load_prof_allocs(session):
 	filename = open("./seed_data/profile_allocations.csv")
@@ -134,23 +144,23 @@ def load_prof_allocs(session):
 		symbol = data[1]
 		ticker_weighting = data[2]
 
-		ticker_id = m_session.query(model.Ticker).filter_by(symbol=
-			symbol).first().id
+		ticker_id = m_session.query(model.Ticker).filter_by(
+					symbol=symbol).first().id
 
-	 	new_profile_allocation = model.ProfileAllocation(risk_profile_id=
-	 		risk_profile_id, ticker_id=ticker_id, ticker_weight_percent=
-	 		ticker_weighting)
+		new_profile_allocation = model.ProfileAllocation(
+					risk_profile_id=risk_profile_id, ticker_id=ticker_id,
+					ticker_weight_percent=ticker_weighting)
 		session.add(new_profile_allocation)
 	session.commit()
 
+
 def main(session):
-	load_ticker_data(build_ticker_url(find_ticker(ticker_list, 
-		"seed_data/ETFs-GOOG.csv")), m_session)
+	load_ticker_data(build_ticker_url(find_ticker(ticker_list,
+				"seed_data/ETFs-GOOG.csv")), m_session)
 	# load_ticker_category(m_session)
 	# calc_percent_change_all(ticker_list, m_session)
 	# load_risk_profs(m_session)
 	# load_prof_allocs(m_session)
 
 if __name__ == "__main__":
-    main(m_session)
-	
+	main(m_session)
