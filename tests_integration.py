eac844beef2cd5333b5867acc3a4d1c792b6e9be
at4260 <at4260@gmail.com>
2015-03-16 13:40:31 -0700

    Integration tests successfully test routes. Fixes #28.

--- tests_integration.py
+++ tests_integration.py
@@ -9,30 +9,61 @@ import model
 
 
 class LoggedInRouting(unittest.TestCase):
-   
+
     def setUp(self):
+        controller.app.config['TESTING'] = True
+        controller.app.config['CSRF_ENABLED'] = False
         self.app = controller.app.test_client()
 
-    def tearDown(self):
-        pass
-
-    def get_user():
-        user = getattr(g, "user", None)
-        if user is None:
-            user = fetch_current_user_from_database()
-            g.user = user
-        return user
-
-    def test_profile(self):
+    def login(self):
         with self.app:
             result = self.app.post("/login", data={
-                "email": "asd@asd.com", "password": "qwerty"})
-            self.assertIn("<h3> Summary </h3>", result.data)
+                "email": "asd@asd.com", "password": "qwerty"},
+                follow_redirects=True)
 
     def test_home(self):
         result = self.app.get("/")
         self.assertIn("Let's Get Started", result.data)
 
-    
+    def test_profile(self):
+        self.login()
+        result = self.app.get("/profile", follow_redirects=True)
+        self.assertIn("Summary", result.data)
+
+    def test_login(self):
+        self.login()
+        result = self.app.get("/results", follow_redirects=True)
+        self.assertIn("Help Me Understand", result.data)
+
+    def test_investments(self):
+        self.login()
+        result = self.app.get("/investments", follow_redirects=True)
+        self.assertIn("Portfolio Allocation", result.data)
+
+
+class LoggedOutRouting(unittest.TestCase):
+
+    def setUp(self):
+        controller.app.config['TESTING'] = True
+        controller.app.config['CSRF_ENABLED'] = False
+        self.app = controller.app.test_client()
+
+    def test_home(self):
+        result = self.app.get("/")
+        self.assertIn("Let's Get Started", result.data)
+
+    def test_profile(self):
+        result = self.app.get("/profile", follow_redirects=True)
+        self.assertIn("Please Sign In", result.data)
+
+    def test_results(self):
+        result = self.app.get("/results", follow_redirects=True)
+        self.assertIn("Please Sign In", result.data)
+
+    def test_investments(self):
+        result = self.app.get("/investments", follow_redirects=True)
+        self.assertIn("Please Sign In", result.data)
+
+
 if __name__ == "__main__":
     unittest.main()
